7535f520897b5e4c1d0b7930ba79b10b669e1d9f
Murat-tunca <murattunca3@gmail.com>
2024-03-13 19:13:19 +0300

    add new tools

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,12 +1,19 @@
 import Title from "./Title";
+import { NavLink } from "react-router-dom";
 
 function Section({ title, more = false, items }) {
   return (
     <section>
       <Title title={title} more={more} />
-      <div className="grid grid-cols-5 gap-x-6"></div>
+      <div className="grid grid-cols-5 gap-x-6">
+        {items.map((item) => (
+          <NavLink key={item.id} to="/" className={"bg-footer p-4 rounded"}>
+            <img src={item.image} />
+            {item.title}
+          </NavLink>
+        ))}
+      </div>
     </section>
   );
 }
-
 export default Section;
